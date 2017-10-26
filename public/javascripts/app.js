0d0a01125ec1e8a05e48b1ce831cbddcaba3153b
Md.Shajidur Rahman Almazee Arnob <arnobxtreme@live.com>
2017-10-26 10:57:21 +0600

    upload

---
+++ public/javascripts/app.js
@@ -0,0 +1,14 @@
+﻿// Get the app module
+var app = angular.module('angularjsNodejsTutorial', []);
+
+// Get the ng-controller
+app.controller('myController', function ($scope, $http) {
+    $scope.data = [];
+    var request = $http.get('/data');
+    request.success(function (data) {
+        $scope.data = data;
+    });
+    request.error(function (data) {
+        console.log('Error: ' + data);
+    });
+});
